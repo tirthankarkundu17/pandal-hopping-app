40cc64e4d05bde85f8ca26def61aaec1cce9b129
Tirthankar Kundu <tirthankar.learning@gmail.com>
2026-02-21 03:08:42 +0530

    update route

--- internal/routes/pandal_route.go
+++ internal/routes/pandal_route.go
@@ -11,5 +11,5 @@ func PandalRoute(router *gin.Engine, handler *handlers.PandalHandler) {
 	router.POST("/pandal", handler.CreatePandal())
 	router.GET("/pandals", handler.GetAllPandals())
 	router.GET("/pandals/pending", handler.GetPendingPandals())
-	router.POST("/pandal/:id/approve", handler.ApprovePandal())
+	router.POST("/pandals/:id/approve", handler.ApprovePandal())
 }
